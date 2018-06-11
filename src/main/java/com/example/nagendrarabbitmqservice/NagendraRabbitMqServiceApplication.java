f02e642028d11f208bedf81199eca0a37b8cd4fa
amar.arawat@gmail.com <amar.arawat@gmail.com>
2018-06-11 19:34:16 +0530

    Initial commit

---
+++ src/main/java/com/example/nagendrarabbitmqservice/NagendraRabbitMqServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.nagendrarabbitmqservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagendraRabbitMqServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagendraRabbitMqServiceApplication.class, args);
+	}
+}
